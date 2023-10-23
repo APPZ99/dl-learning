1a5e1e6770831633da30f674b20998d5214050d0
APPZ99 <925455893@qq.com>
2023-10-23 14:01:59 +0800

    lesson5

--- interpolation.cpp
+++ interpolation.cpp
@@ -3,8 +3,6 @@
 #include "utils.h"
 
 
-
-
 torch::Tensor trilinear_interpolation(torch::Tensor feats, torch::Tensor points){
     
     CHECK_INPUT(feats);
--- test.py
+++ test.py
@@ -1,11 +1,37 @@
 import torch
 import pytorch_cppcuda_learning
 
+def trilinear_interpolation_py(feats, points):
+
+    u = (points[:, 0:1] + 1) / 2
+    v = (points[:, 1:2] + 1) / 2
+    w = (points[:, 2:3] + 1) / 2
+
+    a = (1 - v) * (1 - w)
+    b = (1 - v) * w
+    c = v * (1 - w)
+    d = v * w
+
+    feats_interp = (1 - u) * (a * feats[:, 0] + b * feats[:, 1] +
+                              c * feats[:, 2] + d * feats[:, 3]) + \
+                    u      * (a * feats[:, 4] + b * feats[:, 5] +
+                              c * feats[:, 6] + d * feats[:, 7])
+    
+    return feats_interp
+
+
 if __name__ == '__main__':
     
-    feats = torch.ones(2, device='cuda')
-    points = torch.zeros(2, device='cuda')
+    N = 65536; F = 256
+    feats = torch.rand(N, 8, F, device = 'cuda').requires_grad_()
+    points = torch.rand(N, 3, device = 'cuda')
 
-    out = pytorch_cppcuda_learning.trilinear_interpolation(feats, points)
+    print(points[0, 0], points[0, 1], points[0, 2])
+    print(points[0, 0:1], points[0, 1:2], points[0, 2:3])
+    
+    out_cuda = pytorch_cppcuda_learning.trilinear_interpolation(feats, points)
 
-    print(out)
+    out_py = trilinear_interpolation_py(feats, points)
+
+    print(torch.allclose(out_cuda, out_py))
+
